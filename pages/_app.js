98e1836ac1972d21bcebe7ddbb53b6206831dec1
Catherine Zhao <catherine.zhao.20@dartmouth.edu>
2019-04-06 21:41:39 -0400

    changed server.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,23 +7,6 @@ import { configure } from 'radiks';
 const appConfig = new AppConfig(['store_write', 'publish_data'], 'http://localhost:5000');
 const userSession = new UserSession({ appConfig });
 
-const { User } = require('./models');
-const mongoose = require('mongoose');
-
-console.log('mongoose stuff initialized')
-
-appConfig.use((req, res, next) => {
-  console.log('use for mongoose callback')
-  if (mongoose.connection.readyState) {
-    console.log('if (mongoose.connection.readyState)')
-    next();
-  } else {
-    console.log('else (mongoose.connection.readyState)')
-    require('./mongo')().then(() => next());
-    console.log('else (mongoose.connection.readyState)')
-  }
-});
-
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
     let pageProps = {
--- server.js
+++ server.js
@@ -13,6 +13,23 @@ const handle = app.getRequestHandler();
 
 const port = parseInt(process.env.PORT, 10) || 5000;
 
+const { User } = require('./models');
+const mongoose = require('mongoose');
+
+console.log('mongoose stuff initialized')
+
+appConfig.use((req, res, next) => {
+  console.log('use for mongoose callback')
+  if (mongoose.connection.readyState) {
+    console.log('if (mongoose.connection.readyState)')
+    next();
+  } else {
+    console.log('else (mongoose.connection.readyState)')
+    require('./mongo')().then(() => next());
+    console.log('else (mongoose.connection.readyState)')
+  }
+});
+
 app.prepare().then(async () => {
   const server = express();
   expressWS(server);
