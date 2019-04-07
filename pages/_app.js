d4b39718e02c5f40b6fda5592634b13a9e55b891
Catherine Zhao <catherine.zhao.20@dartmouth.edu>
2019-04-06 21:17:35 -0400

    added mongoose/mongo

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,28 @@
 import App, { Container } from 'next/app';
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, consolidateStreamedStyles } from 'styled-components';
 import { UserSession, AppConfig } from 'blockstack';
 import { configure } from 'radiks';
 
 const appConfig = new AppConfig(['store_write', 'publish_data'], 'http://localhost:5000');
 const userSession = new UserSession({ appConfig });
+
+const { User } = require('.models');
+const mongoose = require('mongoose');
+
+console.log('mongoose stuff initialized')
+
+appConfig.use((req, res, next) => {
+  console.log('use for mongoose callback')
+  if (mongoose.connection.readyState) {
+    console.log('if (mongoose.connection.readyState)')
+    next();
+  } else {
+    console.log('else (mongoose.connection.readyState)')
+    require('./mongo')().then(() => next());
+    console.log('else (mongoose.connection.readyState)')
+  }
+});
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
